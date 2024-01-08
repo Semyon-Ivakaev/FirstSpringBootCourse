95ce9058d44ae90ecc971d0f6ef16f20ea3c4c30
Semyon-Ivakaev <stixonforpython@gmail.com>
2024-01-08 20:44:03 +0300

    Initial commit

---
+++ src/main/java/com/springacademy/FirstSpringBootCourse/FirstSpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package com.springacademy.FirstSpringBootCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootCourseApplication.class, args);
+	}
+
+}
